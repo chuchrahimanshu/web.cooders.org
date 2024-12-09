a403911b26d68bf322c7938a13474c5c61f28a26
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-12-09 21:01:54 +0530

    Integrate Themes in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "base-background": "var(--base-background)",
+        "overlay-background": "var(--overlay-background)",
+        "base-border": "var(--base-border)",
+        "highlight-border": "var(--highlight-border)",
+        "action-button": "var(--action-button)",
+        "base-text": "var(--base-text)",
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    scrollbar: ["rounded"],
+  },
+  plugins: [scrollbar({ nocompatible: true })],
 };
