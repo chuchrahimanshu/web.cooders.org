d905afeacfab9fdd673a06965279f559ca044c72
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-12-10 16:14:46 +0530

    Update the Accounts Functionality

--- src/components/form/TwoColumnInput.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-
-const TwoColumnInput: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <section className="md:flex md:gap-5">{children}</section>;
-};
-
-export default TwoColumnInput;
