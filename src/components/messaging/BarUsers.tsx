9ab49fa730e6827566561f11b06c55b4b0dc8473
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-12-13 10:08:57 +0530

    Design and Style Messaging Chat Header

--- src/components/messaging/BarUsers.tsx
+++ src/components/messaging/BarUsers.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import { BiDotsVerticalRounded } from 'react-icons/bi'
+import React from "react";
 
 const BarUsers: React.FC = () => {
-
   const users = [
     {
       id: 1,
@@ -10,7 +8,7 @@ const BarUsers: React.FC = () => {
       fullName: "Himanshu Chuchra",
       lastMessage: "This is my last message where the chat was ended.",
       messageTime: "10:24 AM",
-      isPinned: false
+      isPinned: false,
     },
     {
       id: 2,
@@ -18,31 +16,43 @@ const BarUsers: React.FC = () => {
       fullName: "Satyam Chuchra",
       lastMessage: "This is my last message where the chat was ended.",
       messageTime: "10:24 AM",
-      isPinned: true
-    }
-  ]
+      isPinned: true,
+    },
+  ];
 
   return (
-    <div className='h-full pt-[6px]'>
-      {users && users.length > 0 && users.map((user) => (
-        <div key={user.id} className='flex justify-between px-3 py-[10px] hover:bg-base-border transition-all duration-300 ease-in-out cursor-pointer'>
-          <div className='flex gap-2 items-center w-[80%]'>
-            <img src={user.image} alt="User" className='h-11 w-11 rounded-full object-cover' />
-            <div className='flex flex-col gap-0'>
-              <p className='font-medium'>{user.fullName}</p>
-              <p className='text-[13px] line-clamp-1'>{user.lastMessage}</p>
+    <div className="h-full pt-[6px]">
+      {users &&
+        users.length > 0 &&
+        users.map((user) => (
+          <div
+            key={user.id}
+            className="flex justify-between px-3 py-[10px] hover:bg-base-border transition-all duration-300 ease-in-out cursor-pointer">
+            <div className="flex gap-2 items-center w-[80%]">
+              <img
+                src={user.image}
+                alt="User"
+                className="h-11 w-11 rounded-full object-cover"
+              />
+              <div className="flex flex-col gap-0">
+                <p className="font-medium">{user.fullName}</p>
+                <p className="text-[13px] line-clamp-1">{user.lastMessage}</p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-start pt-[2px] gap-[6px]">
+              <p className="text-[10px] font-medium opacity-75 self-end">
+                {user.messageTime}
+              </p>
+              <div className="self-end bg-action-button h-5 w-5 flex items-center justify-center rounded-full opacity-70">
+                <p className="text-[14px] font-medium -ml-[1px] text-white">
+                  1
+                </p>
+              </div>
             </div>
           </div>
-          <div className='flex flex-col justify-start pt-[2px] gap-[6px]'>
-            <p className='text-[10px] font-medium opacity-75 self-end'>{user.messageTime}</p>
-            <div className='self-end bg-action-button h-5 w-5 flex items-center justify-center rounded-full opacity-70'>
-              <p className='text-[14px] font-medium -ml-[1px] text-white'>1</p>
-            </div>
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default BarUsers
+export default BarUsers;
--- src/components/messaging/ChatHeader.tsx
+++ src/components/messaging/ChatHeader.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
 const ChatHeader: React.FC = () => {
+  const user = {
+    id: 1,
+    image: "https://github.com/shadcn.png",
+    fullName: "Himanshu Chuchra",
+    username: "@himanshuchuchra",
+    messageTime: "10:24 AM",
+    isPinned: false,
+  };
+
   return (
-    <div className='h-[75px] border-b border-b-base-border'></div>
+    <>
+      {user &&
+        <div
+          key={user.id}
+          className="h-[75px] border-b border-b-base-border flex justify-between px-3 py-[10px] hover:bg-base-border transition-all duration-300 ease-in-out cursor-pointer">
+          <div className="flex gap-2 items-center w-[80%]">
+            <img
+              src={user.image}
+              alt="User"
+              className="h-11 w-11 rounded-full object-cover"
+            />
+            <div className="flex flex-col gap-0">
+              <p className="text-[17px] font-medium line-clamp-1">{user.fullName}</p>
+              <p className="text-[14px] opacity-70 line-clamp-1">{user.username}</p>
+            </div>
+          </div>
+          
+        </div>
+      }
+    </>
   )
 }
 
