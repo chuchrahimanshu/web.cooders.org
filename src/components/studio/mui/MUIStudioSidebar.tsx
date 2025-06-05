7baaf885f59b4e22760915f0547712492cc63d8c
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-06-05 23:07:21 +0530

    Design Sidebar Header Component

--- src/components/studio/mui/MUIStudioSidebar.tsx
+++ src/components/studio/mui/MUIStudioSidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Divider, Typography } from "@mui/material";
 import { MUI_COMPONENTS } from "../../../data";
 import ComponentItem from "./ComponentItem";
+import SidebarHeader from "./SidebarHeader";
 
 const MUIStudioSidebar: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const MUIStudioSidebar: React.FC = () => {
       className="h-[calc(100vh-65px)] w-[240px] flex flex-row items-stretch bg-background"
     >
       <Box
-        className="w-full h-full overflow-y-auto p-3 hide-scrollbar"
+        className="w-full h-full"
         sx={{
           scrollbarWidth: "none",
           "&::-webkit-scrollbar": {
@@ -18,30 +19,37 @@ const MUIStudioSidebar: React.FC = () => {
           },
         }}
       >
-        {MUI_COMPONENTS.map((group, groupIndex) => (
-          <Box key={groupIndex} className="mb-4">
-            <Typography
-              sx={{
-                fontSize: 13.5,
-                fontWeight: 600,
-                opacity: 0.5,
-                mb: 1.5,
-              }}
-            >
-              {group.header.toUpperCase()}
-            </Typography>
-            {group.contents.map((compItem, compIndex) => (
-              <ComponentItem
-                key={`${groupIndex}-${compIndex}`}
-                compIndex={compIndex}
-                compItem={compItem}
-                index={groupIndex}
-              />
-            ))}
-          </Box>
-        ))}
+        <SidebarHeader />
+        <Box className="w-full h-[calc(100vh-120px)] overflow-y-auto hide-scrollbar pt-3">
+          {MUI_COMPONENTS.map((group, groupIndex) => (
+            <Box key={groupIndex} className="mb-4 px-3">
+              <Typography
+                sx={{
+                  fontSize: 13.5,
+                  fontWeight: 600,
+                  opacity: 0.5,
+                  mb: 1.5,
+                }}
+              >
+                {group.header.toUpperCase()}
+              </Typography>
+              {group.contents.map((compItem, compIndex) => (
+                <ComponentItem
+                  key={`${groupIndex}-${compIndex}`}
+                  compIndex={compIndex}
+                  compItem={compItem}
+                  index={groupIndex}
+                />
+              ))}
+            </Box>
+          ))}
+        </Box>
       </Box>
-      <Divider orientation="vertical" flexItem />
+      <Divider
+        orientation="vertical"
+        flexItem
+        sx={{ height: "100%", alignSelf: "stretch" }}
+      />
     </Box>
   );
 };
